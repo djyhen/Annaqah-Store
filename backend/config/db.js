acdb8661f426c6e5e805d70ba4952909135d37a3
jyhen <jyhen@gmail.com>
2024-08-29 12:59:28 +0100

    last commit for backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://jyhentouri651:i8d0IW3rnQoFDeqs@cluster0.mjlyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("DB Connection Error:", error);
